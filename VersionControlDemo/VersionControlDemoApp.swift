8dccadba7205f7c58b0226f90d6bd71d9734e392
TylerLawrence <tyler.lawrence15@gmail.com>
2021-11-29 15:26:16 -0500

    Initial Commit

---
+++ VersionControlDemo/VersionControlDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VersionControlDemoApp.swift
+//  VersionControlDemo
+//
+//  Created by Tyler Lawrence on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct VersionControlDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
