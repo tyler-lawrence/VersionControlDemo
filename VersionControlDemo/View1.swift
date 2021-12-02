dbb5d3a1b7566b694c9bc4889a52577e528e0028
DocHill <mdh830531@gmail.com>
2021-12-02 12:05:38 -0500

    h

--- VersionControlDemo/View1.swift
+++ VersionControlDemo/View1.swift
@@ -9,11 +9,15 @@ import SwiftUI
 
 struct View1: View {
     var body: some View {
+        ZStack{
+            Color.red
+        
+        
         Text("riq's treats make me friq!")
             .padding()
     }
 }
-
+}
 struct View1_Previews: PreviewProvider {
     static var previews: some View {
         View1()
