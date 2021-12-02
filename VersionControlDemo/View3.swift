594d429f6bc7a24cbab20683af14f8a25356cb32
Jonathan Cameron <ethyl256@gmail.com>
2021-12-02 11:51:44 -0500

    added dog picture and description

--- VersionControlDemo/View3.swift
+++ VersionControlDemo/View3.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct View3: View {
     var body: some View {
-        Text("View3")
+        VStack  {
+        Image("dog")
+            .resizable()
+            .scaledToFit()
+        Text("this is a dog")
+            .font(.title2)
+            .fontWeight(.semibold)
+        }
     }
 }
 
