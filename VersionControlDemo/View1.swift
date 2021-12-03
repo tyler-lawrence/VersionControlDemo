90bcbf0ba9df0a93d39ea2dafad22d91f238f704
TahiraLasker <tlasker121@gmail.com>
2021-12-03 09:44:44 -0500

    reformatted text on view 1 to large

--- VersionControlDemo/View1.swift
+++ VersionControlDemo/View1.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct View1: View {
     var body: some View {
+        VStack {
         Text("View1")
+        Text("Happy Friday")
     }
 }
-
+}
 struct View1_Previews: PreviewProvider {
     static var previews: some View {
         View1()
