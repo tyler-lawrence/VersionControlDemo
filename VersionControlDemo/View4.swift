726235701690adaedae4593d51d52d076018437c
TylerLawrence <tyler.lawrence15@gmail.com>
2021-12-08 13:51:58 -0500

    added new file

---
+++ VersionControlDemo/View4.swift
@@ -0,0 +1,20 @@
+//
+//  View4.swift
+//  VersionControlDemo
+//
+//  Created by Tyler Lawrence on 12/8/21.
+//
+
+import SwiftUI
+
+struct View4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct View4_Previews: PreviewProvider {
+    static var previews: some View {
+        View4()
+    }
+}
